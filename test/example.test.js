fd6df185f6537c947698fae46aa980176035fd1a
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-13 17:54:16 -0800

    add isYes and test of isYes

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,19 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-
+import isYes from '../isYes.js';
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+QUnit.module("Test for isYes");
+
+test('YES, y, ya, should be true', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    const input = 'Ya';
+    const expected = true;
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const result = isYes(input);
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(result, expected);
 });
